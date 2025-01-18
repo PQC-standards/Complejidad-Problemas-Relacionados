b3fbb6ebe4fd32a256d5d89f1a7dea56cdb27b8e
GabriVG <gabrivacarog@gmail.com>
2025-01-18 16:46:51 +0100

    Comit de prueba

--- LatticesComplexity/src/tests/TestLattices.java
+++ LatticesComplexity/src/tests/TestLattices.java
@@ -18,7 +18,7 @@ import us.lsi.graphics.MatPlotLib;
 
 public class TestLattices {
 
-	//Prueba commit
+	
     private static Integer nMin = 1;  // Dimensión mínima
     private static Integer nMax = 4; // Dimensión máxima
     private static Integer nIncr = 1; // Incremento de dimensión
